e3b8025a744eb5fcb22b78432f200aea5c3edcd5
Nero <nelsonnnnn.junior@gmail.com>
2025-08-28 13:33:33 -0300

    Apenas as seções clicadas serão visiveis

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-// Verifica quando o site estiver carregado, e gera o botao que permite mudar o tema do site
+// Verifica quando o site estiver carregado
 document.addEventListener("DOMContentLoaded", () => {
   const themeBtn = document.getElementById("mudarTema")
   const body = document.body
+  const sections = document.querySelectorAll("main section")
+  const linksMenu = document.querySelectorAll(".menu a")
 
+
+ // Verifica quando o botão é clicado
   themeBtn.addEventListener("click", () =>{
     body.classList.toggle("dark-theme")
 
@@ -16,5 +20,28 @@ document.addEventListener("DOMContentLoaded", () => {
   })
   
 
+//Garante que so seja vista a seção que o usuário clique
+
+function hideSections() {
+  sections.forEach(section => {
+    section.style.display = "none"
+  })
+}
+
+function viewSection(id){
+  hideSections()
+  const section = document.querySelector(id)
+  if (section) section.style.display = "block"
+}
+
+hideSections()
+viewSection("#sobre")
+
+linksMenu.forEach(link => {
+  link.addEventListener("click", event => {
+    const href = link.getAttribute("href") // Verifica para onde o link leva
+    viewSection(href)
+  })
+})
 
 })
